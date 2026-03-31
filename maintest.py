0ccce50d61b6b0dafd9efc8d0bdb02ec03d8110f
SUTHAHER Hemadhuran <dhuran619@gmail.com>
2026-03-31 16:11:58 +0200

    Create maintest.py

---
+++ maintest.py
@@ -0,0 +1,10 @@
+#c'est qu'un doissier test ne pas l'effacer
+# Ajoute les composant:
+
+import tkinter as tk 
+from tkinter import ttk, messagebox, simpledialog
+import json
+import os
+from datetime import datetime, date
+import calendar
+
