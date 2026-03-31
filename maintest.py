9e78b29f2f6d86d5c605ebd78647f6939a9eaae9
Illampooran05 <illampooran.kusalarajah@gmail.com>
2026-03-31 16:20:17 +0200

    edit

--- maintest.py
+++ maintest.py
@@ -8,3 +8,16 @@ import os
 from datetime import datetime, date
 import calendar
 
+
+# 1. Importation des modules
+
+# 2. Initialisation des données
+
+# 3. Création des menus d'affichage
+
+# 4. Gestion des entrées (Saisie utilisateur)
+
+# 5. Boucle de contrôle principale (while True)
+
+# 6. Gestion des erreurs d'affichage
+
